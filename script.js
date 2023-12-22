dc07512fd3580163b4ca063d8ab8714457e372aa
Bashan Sandeepa <bashansandeepa@gmail.com>
2023-12-23 00:04:38 +0530

    getting first day

--- script.js
+++ script.js
@@ -9,12 +9,17 @@ currMonth = date.getMonth();
 const months = ["January", "February", "March","April", "May", "June", "July", "August", "September","October","November", "December"] ;
 
 const renderCalendar = ()=> {
-    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
     let liTag = "";
 
-for(let i=1; i<= lastDateofMonth; i++){
-    liTag += `<li>${i}</li>`;
-}
+    for(let i = firstDayofMonth; i>0; i--){
+        liTag += `<li>${i}</li>`;
+    }
+
+    for(let i=1; i<= lastDateofMonth; i++){
+        liTag += `<li>${i}</li>`;
+    }
     
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
